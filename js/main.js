23bc31d9b51e3bf820cfd1f750ae11a266f22f3e
alsomslo <133613694+alsomslo@users.noreply.github.com>
2023-07-11 18:22:37 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,4 +7,21 @@ $(function () {
     $(".mainslide").slick({
         arrows: false,
     })
+
+
+    $('.mobile_btn').on('click', function () {
+        $(this).toggleClass('on');
+        $('.gnb').toggleClass('on');
+    });
+
+    $('.gnb>ul>li>a').on('click', function (e) {
+        e.preventDefault();
+        $(this).next().stop().slideDown();
+        $(this).parent().siblings().find('.sub').stop().slideUp();
+    });
+
+    $(window).on('resize', function () {
+        $('.gnb .sub').removeAttr('style')
+    })
+
 })
